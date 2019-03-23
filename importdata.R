bb02a50e327c8e9ccbfef491a78a662ab4234564
unknown <a3062930629@gmail.com>
2019-03-23 22:29:25 +0800

    0323

--- importdata.R
+++ importdata.R
@@ -5,7 +5,7 @@
 #-----------------------------------------------------------------------------
 # Using read.table to import .txt file
 rm(list=ls())
-
+etf4<-read.table("ETF4_2000_2018_d.txt")
 # you will get error message! This is because Chinese characters that cannot be recognized!
 # But this works fine in Mac!!!
 # But you can add: fileEncoding = 'UTF-8-BOM' to solve the problem!
@@ -24,8 +24,8 @@ head(etf4)
 etf4<-read.csv("ETF4_2000_2018_d.csv", colClasses = c("證券代碼"="character"))
 str(etf4)
 # If you get garbled text, you may try:
-etf4.csv<-read.csv("ETF4_2000_2018_d.csv", fileEncoding='big5',
-                   colClasses=c('factor','factor','factor','numeric','numeric'))
+etf4.csv<-read.csv("ETF4_2000_2018_d.csv", fileEncoding='big5', 
+                   colClasses=c('factor', 'factor', 'factor', 'numeric', 'numeric'))
 head(etf4.csv)
 str(etf4.csv)
 # using read_csv to imoprt data to tibble format
@@ -40,8 +40,49 @@ str(etf4_csv)
 install.packages("readxl")
 library(readxl)
 etf4_xls<-read_excel("ETF4_2000_2018_d.xls", 
-                     col_types =c("text","text","text","numeric","numeric"))
+                     col_types =c("text", "text","text", "numeric","numeric"))
 head(etf4_xls)
+#----------------------------------------------------------------------------
+# Practice 01:
+# Try to import '2017Q4_code.csv'
+#-----------------------------------------------------------------------------
+
+tw50_2017<-read_csv("2017Q4_code.csv", locale = locale(encoding='big5'))
+head(tw50_2017)
+
+#
+install.packages("quantmod")
+library(quantmod)
+
+tickers<-c("2330.TW", "1101.TW", "1102.TW")
+getSymbols(tickers, from= '2016-01-01', auto.assign = T)
+#
+code50<-tw50_2017$code
+code50.tw <- paste(code50, ".TW", sep="")
+#
+data=new.env()
+getSymbols(code50.tw, from= '2016-01-01', env = data, auto.assign = T)
+names(data)
+data$`1101.TW`
+# Error: 2311.TW download failed after two attempts. Error message:
+# Replace 2311 with 3711
+getSymbols("3711.TW", from= '2016-01-01', auto.assign = T)
+data$"3711.TW"<-`3711.TW`
+# Combine adjusted closing price across all 50 stocks!
+str(data)
+ls(data)
+tw50p<-data.frame()
+
+t<-as.data.frame(as.list(data))
+dim(t)
+head(t)
+head(t[1])
+
+for (i in c("1101.TW", "1102.TW")) {
+    tmp<-paste(data)
+    tw50p<-cbind(tw50p, Ad(data$"1101.TW"))
+}
+
 #=============================================================================
 # clean data
 etf4.c<-etf4_csv[, c(-2, -4)]
@@ -49,14 +90,14 @@ etf4.c<-etf4.c[-1,]
 colnames(etf4.c)<-c("id", "date", "price")
 # use pipe operator 
 library(magrittr)
-install.packages("dplyr")
+#install.packages("dplyr")
 library(dplyr)
 etf4.c<-etf4_csv%>%select(c(1,3,5))%>%rename("id" = "證券代碼", "date"= "日期", "price" = "當日均價(元)")
 etf4.c
 #etf4.c<-etf4_csv%>%select("證券代碼", "日期", "當日均價(元)")
 #-----------------------------------------------------------------                
 # use dcast to reorder dataframe by date;
-install.packages("reshape2")
+#install.packages("reshape2")
 library(reshape2)
 etf4.reorder = dcast(etf4.c, date~id)
 dim(etf4.reorder)
@@ -68,7 +109,7 @@ head(etf4.reorder)
 str(etf4.reorder)
 # convert character into numeric 
 # convert to xts
-install.packages("xts")
+#install.packages("xts")
 library(xts)
 etf4.xts<-xts(etf4.reorder[,-1], order.by = etf4.reorder$date)
 head(etf4.xts)
@@ -90,7 +131,7 @@ head(etf4.xts.fill)
 etf4.xts<-na.omit(etf4.xts)
 head(etf4.xts)
 # or complete cases
-install.packages("tidyr")
+#install.packages("tidyr")
 library(tidyr)
 etf4.xts1<-etf4.xts[complete.cases(etf4.xts),]
 head(etf4.xts1)
@@ -129,9 +170,6 @@ last(lastweek, 2)
 # Extract all but the first two days of lastweek
 first(lastweek, "-2 days")
 
-
-
-
 #------------------------------------------------------------
 # Converting Daily Prices to Monthly Returns in the xts world
 #------------------------------------------------------------
@@ -159,19 +197,56 @@ dim(etf4_returns_xts)
 # you can also use coredata() to compute returns directly
 etf4_ret<-coredata(etf4_monthly[-1,])/coredata(etf4_monthly[-dim(etf4_monthly)[1],])-1
 head(etf4_ret)
+class(etf4_ret)
 #============================================================
 # Plot in R
 #-------------------------------------------------------------
 plot(etf4_returns_xts, xaxt='n')
 axis(1, index(etf4_returns_xts), format(index(etf4_returns_xts), "%Y/%m"))
-#axis(side=1, at=yahoo2$date[ at ], labels=format(yahoo2$date[at], '%b-%y'))
-#plot.xts(etf4_mon_ret, auto.legend = TRUE)
+# plot the scatterplot of 0050 and 00646
+# convert xts into df using fortify()
+etf4_ret.df1<-fortify(etf4_returns_xts)
+head(etf4_ret.df1)
+plot(etf4_ret.df1$`0050`, etf4_ret.df1$`00646`, pch=20,
+     col = 'darkred', main = '0050 vs. 00646 monthly returns',
+     xlab = '0050', ylab = '00646 S&P500')
 #-----------------------------------------------------------
 #install.packages("tidyverse")
 library(tidyverse)
 library(ggplot2)
 # convert xts into data frame which can be used by ggplot
-#etf4_returns.df<-fortify(etf4_returns_xts)
+# split date index in xts into year, month and day columns 
+# using lubridate package
+library(lubridate)
+etf4_ret.df2 <- cbind(etf4_ret.df1, month=month(index(etf4_returns_xts)), 
+                      year=year(index(etf4_returns_xts)))
+#
+ggplot(data = etf4_ret.df2) +
+  geom_point(mapping = aes(x = etf4_ret.df2$`0050`, y = etf4_ret.df2$`0056`, color = month))
+#
+ggplot(data = etf4_ret.df2) +
+  geom_point(mapping = aes(x = etf4_ret.df2$`0050`, y = etf4_ret.df2$`0056`, size = month))
+#
+ggplot(data = etf4_ret.df2) +
+  geom_point(mapping = aes(x = etf4_ret.df2$`0050`, y = etf4_ret.df2$`0056`, alpha = month))
+#
+etf4_ret
+etf4_ret.tmp<-data.frame(date = index(etf4_returns_xts), etf4_ret)
+head(etf4_ret.tmp)
+# or you can use the following code
+etf4_ret.tmp<-etf4_returns_xts %>% 
+  data.frame(date=index(.)) %>% 
+  remove_rownames() %>% 
+  gather(asset, return, -date) # turn data into long format
+
+head(etf4_ret.tmp)
+#
+plot(etf4_ret.tmp$X0050, etf4_ret.tmp$X0056)
+#
+ggplot(etf4_ret.tmp) +
+  geom_point(mapping = aes(x = etf4_ret.tmp$`0050`, y = etf4_ret.tmp$`0056`))
+#
+
 etf4_ret.df<-fortify(etf4_returns_xts, melt=TRUE)
 head(etf4_ret.df)
 #
@@ -209,39 +284,10 @@ etf4_ret.df %>%
   ylab("distribution") +
   theme_update(plot.title = element_text(hjust = 0.5))
 
-# plot the scatterplot of 0050 and 00646
-# convert xts into df 
-etf4_ret.df1<-fortify(etf4_returns_xts)
-plot(etf4_returns.df1$`0050`, etf4_returns.df1$`00646`, pch=20,
-     col = 'darkred', main = '0050 vs. 00646 monthly returns',
-     xlab = '0050', ylab = '00646 S&P500')
+#---------------------------------------------------------------
 
 
-#================================================
-# some manipulations on xts 
-#================================================
-dates <- as.Date("2016-01-01") + 0:40
-# Create ts_a
-ts_a <- xts(x = 1:41, order.by = dates)
-ts_a
-# Create ts_b
-ts_b <- xts(x = 1:41, order.by = as.POSIXct(dates))
-ts_b
-ts_a[index(ts_a)]
-ts_a[index(ts_b)]
-#
-tmp_file <- "http://s3.amazonaws.com/assets.datacamp.com/production/course_1127/datasets/tmp_file.csv"
-# Create dat by reading tmp_file
-dat <- read.csv(tmp_file)
-head(dat)
-str(dat)
-# Convert dat into xts
-xts(dat, order.by = as.Date(rownames(dat), "%m/%d/%Y"))
 
-# Read tmp_file using read.zoo
-dat_zoo <- read.zoo(tmp_file, index.column = 0, sep = ",", format = "%m/%d/%Y")
-dat_zoo
-str(dat_zoo)
-# Convert dat_zoo to xts
-dat_xts <- as.xts(dat_zoo)
-#----------------------------------------------
+
+
+
